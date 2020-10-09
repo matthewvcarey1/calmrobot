ed49e66a9353276f1fca46124775781d75d9f6f6
Matthew Carey <Matthew.Carey@gbgplc.com>
2020-10-09 08:21:41 +0100

    Change things about

--- cmd/calmrobot/main.go
+++ cmd/calmrobot/main.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 
 	"github.com/matthewvcarey1/calmrobot/internal/pkg/mapland"
-	"github.com/matthewvcarey1/calmrobot/internal/pkg/robot"
+	"github.com/matthewvcarey1/calmrobot/internal/pkg/robotview"
 )
 
 func main() {
 	// 789 is chosen as 7+8+9=24
 	// Every coordinate with 789 in will be a mine
 	land := mapland.New(789*2 + 1)
-	robot := robot.New(land)
-	robot.MarkMines()
+	rv := robotview.New(land)
+	rv.MarkMines()
 	land.Draw()
 	// Flood fill the from the centre of the map
-	count := robot.FloodFill(0, 0)
+	count := rv.FloodFill(0, 0)
 	fmt.Println("Count", count)
 	land.Draw()
 	//fmt.Println("Count", land.Count())
--- internal/pkg/robotview/robotview.go
+++ internal/pkg/robotview/robotview.go
@@ -1,10 +1,10 @@
-package robot
+package robotview
 
 import (
-	"github.com/matthewvcarey1/calmrobot/internal/pkg/mapland"
+	"github.com/matthewvcarey1/calmrobot/pkg/internal/pkg/mapland"
 )
 
-type Robot struct {
+type RobotView struct {
 	land *mapland.MapLand
 }
 
@@ -42,16 +42,16 @@ func isSafe(x int, y int) bool {
 	return (sum(digits) <= safe)
 }
 
-func New(land *mapland.MapLand) *Robot {
-	return &Robot{land: land}
+func New(land *mapland.MapLand) *RobotViewView {
+	return &RobotView{land: land}
 }
 
-type pair struct {
+type robot struct {
 	x int
 	y int
 }
 
-func (r *Robot) MarkMines() {
+func (r *RobotView) MarkMines() {
 	x1, y1, x2, y2 := r.land.GetMinMax()
 	for x := x1; x < x2; x++ {
 		for y := y1; y < y2; y++ {
@@ -64,55 +64,54 @@ func (r *Robot) MarkMines() {
 	}
 }
 
-func east(p pair)pair{
-	return pair{p.x+1, p.y}
+func (r robot) east() robot {
+	return robot{r.x + 1, r.y}
 }
 
-func west(p pair) pair{
-	return  pair{p.x-1, p.y}
+func (r robot) west() robot {
+	return robot{r.x - 1, r.y}
 }
 
-func south(p pair)pair{
-	return pair{p.x, p.y-1}
+func (r robot) south() robot {
+	return robot{r.x, r.y - 1}
 }
 
-func north(p pair)pair{
-	return pair{p.x, p.y+1}
+func (r robot) north() robot {
+	return robot{r.x, r.y + 1}
 }
 
-
-func (r *Robot) FloodFill(x int, y int) int {
-	var queue []pair
+func (rv *RobotView) FloodFill(x int, y int) int {
+	var queue []robot
 	count := int(0)
-	if r.land.Get(x, y) != 0x20 {
+	if rv.land.Get(x, y) != 0x20 {
 		return 0
 	}
-	r.land.Set(x, y)
+	rv.land.Set(x, y)
 	count++
-	queue = append(queue, pair{
+	queue = append(queue, robot{
 		x: x,
 		y: y,
 	})
 	for len(queue) > 0 {
-		p := queue[0]     // first element
+		r := queue[0]     // first element
 		queue = queue[1:] //Dequeue
-		if w:=west(p); r.land.Get(w.x,w.y) == 0x20 {
-			r.land.Set(w.x, w.y)
+		if w := r.west(); rv.land.Get(w.x, w.y) == 0x20 {
+			rv.land.Set(w.x, w.y)
 			count++
 			queue = append(queue, w)
 		}
-		if e:=east(p); r.land.Get(e.x, e.y) == 0x20 {
-			r.land.Set(e.x, e.y)
+		if e := r.east(); rv.land.Get(e.x, e.y) == 0x20 {
+			rv.land.Set(e.x, e.y)
 			count++
 			queue = append(queue, e)
 		}
-		if n:=north(p); r.land.Get(n.x, n.y) == 0x20 {
-			r.land.Set(n.x, n.y)
+		if n := r.north(); rv.land.Get(n.x, n.y) == 0x20 {
+			rv.land.Set(n.x, n.y)
 			count++
 			queue = append(queue, n)
 		}
-		if s:=south(p); r.land.Get(s.x, s.y) == 0x20 {
-			r.land.Set(s.x, s.y)
+		if s := r.south(); rv.land.Get(s.x, s.y) == 0x20 {
+			rv.land.Set(s.x, s.y)
 			count++
 			queue = append(queue, s)
 		}
