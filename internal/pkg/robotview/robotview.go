12df8c8fb5feb2ac152c385e1b1d3785364eb030
Matthew Carey <matthewvcarey@gmail.com>
2020-11-15 12:22:43 +0000

    Much quicker simplifying isSafe function

--- internal/pkg/robotview/robotview.go
+++ internal/pkg/robotview/robotview.go
@@ -12,42 +12,27 @@ type RobotView struct {
 	land *mapland.MapLand
 }
 
-//const (
-// The safe threshold
-//	safe = 23
-//)
-
-// Get array of digits in a positive or negative decimal number
-func getDigits(num int) []int {
-	var res []int
+// SumDigits sum the absolute decimal digits in a number
+func sumDigits(num int) int {
+	res := 0
 	if num < 0 {
 		num = -num
 	}
 	if num == 0 {
-		res = append(res, 0)
+		return res
 	} else {
 		for ; num > 0; num = num / 10 {
 			digit := num % 10
-			res = append(res, digit)
+			res += digit
 		}
-	}
-	return res
-}
-
-// sum an array of ints
-func sum(arr []int) int {
-	res := int(0)
-	for _, v := range arr {
-		res += v
 	}
 	return res
 }
 
 // isSafe checks whether a coordinate is safe
 func (rv *RobotView) isSafe(x int, y int) bool {
-	digits := getDigits(x)
-	digits = append(digits, getDigits(y)...)
-	return (sum(digits) <= rv.safe)
+	val := sumDigits(x) + sumDigits(y)
+	return val <= rv.safe
 }
 
 // New RobotView
@@ -61,15 +46,15 @@ type robot struct {
 	y int
 }
 
-// Mark all the mines in the land
-func (r *RobotView) MarkMines() {
-	x1, y1, x2, y2 := r.land.GetStartEnd()
+// MarkMines mark all the mines in the land
+func (rv *RobotView) MarkMines() {
+	x1, y1, x2, y2 := rv.land.GetStartEnd()
 	for x := x1; x < x2; x++ {
 		for y := y1; y < y2; y++ {
-			if !r.isSafe(x, y) {
-				r.land.SetMine(x, y)
+			if !rv.isSafe(x, y) {
+				rv.land.SetMine(x, y)
 			} else {
-				r.land.SetClear(x, y)
+				rv.land.SetClear(x, y)
 			}
 		}
 	}
@@ -153,7 +138,6 @@ func newRobotQueue() *robotQueue {
 // by the robot and returns the number of coordinates it contains
 func (rv *RobotView) FloodFill(x int, y int) (int, error) {
 	queue := newRobotQueue()
-	//x1, y1, x2, y2 := rv.land.GetStartEnd()
 	count := int(0)
 	if !rv.land.IsClear(x, y) {
 		return 0, nil
@@ -166,9 +150,7 @@ func (rv *RobotView) FloodFill(x int, y int) (int, error) {
 	})
 	for queue.size() > 0 {
 		r := queue.pop()
-		//if r.x == x1 || r.x == x2-1 || r.y == y1 || r.y == y2-1 {
-		//	return 0, errors.New("FloodFill has reached the edge of the world, the world needs to be bigger")
-		//}
+
 		if w := r.west(); rv.land.IsClear(w.coords()) {
 			rv.land.SetAccessable(w.coords())
 			count++
