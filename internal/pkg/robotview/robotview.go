339480b714d10ae3030bd85d248e3d8c30bea3d9
Matthew Carey <matthewvcarey@gmail.com>
2020-11-16 16:06:46 +0000

    Tidy silly case out

--- internal/pkg/robotview/robotview.go
+++ internal/pkg/robotview/robotview.go
@@ -18,13 +18,9 @@ func sumDigits(num int) int {
 	if num < 0 {
 		num = -num
 	}
-	if num == 0 {
-		return res
-	} else {
-		for ; num > 0; num = num / 10 {
-			digit := num % 10
-			res += digit
-		}
+	for ; num > 0; num = num / 10 {
+		digit := num % 10
+		res += digit
 	}
 	return res
 }
