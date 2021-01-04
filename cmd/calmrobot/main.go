e41224e8a9dcd191c9472cf489097b855dbd0445
Matthew Carey <mcarey@calmid.com>
2021-01-04 16:34:34 +0000

    Remove verbose parameter

--- cmd/calmrobot/main.go
+++ cmd/calmrobot/main.go
@@ -14,7 +14,6 @@ const (
 	quadrants = 4
 )
 
-var verbose = flag.Bool("verbose", false, "prints large maps of the land before and after the flood fill, best pipe to a file")
 var images = flag.Bool("images", false, "generates png files")
 var safe = flag.Int("safe", 23, "safe value for robot")
 
@@ -24,15 +23,13 @@ func main() {
 		fmt.Println("Time taken:", time.Now().Sub(t1))
 	}()
 	flag.Parse()
-	// 1400x1400 is the smallest world that will work (699 * 2 + 2)
-	// 6 + 9 + 9 = 24 even one coordinate will cause a mine
+	// 1400x1400 is the smallest world that will work for a safe value of 23 (699 * 2 + 2)
+	// 6 + 9 + 9 = 24 means that even one coordinate will cause a mine
 	land := mapland.New(*safe)
 	rv := robotview.New(*safe, land)
 	// Mark all the mines on the map
 	rv.MarkMines()
-	if *verbose {
-		land.Draw()
-	}
+
 	if *images {
 		land.DrawImage("mines.png")
 	}
@@ -46,13 +43,9 @@ func main() {
 	}
 	axisfill := mapland.CalcMaplandSize(*safe) - 1
 	count = (count-axisfill)*quadrants + origin
-	if *verbose {
-		fmt.Println()
-		land.Draw()
-	}
+
 	if *images {
 		land.DrawImage("robot.png")
 	}
 	fmt.Println("The robot can access", count, "coordinates")
-
 }
